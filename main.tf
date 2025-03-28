2cab21556b53b4835db2917b62836f498934f00d
Anastasiia Tabalova <anastasiia869@bailliegifford.com>
2025-03-28 14:43:22 +0000

    outputs + cluster

--- main.tf
+++ main.tf
@@ -2,3 +2,24 @@ resource "azurerm_resource_group" "example" {
   name     = "nastya_group"
   location = "West Europe"
 }
+
+resource "azurerm_kubernetes_cluster" "aks-cluster" {
+  name                = "nastya-aks-cluster"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+#   dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}

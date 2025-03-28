affb3eeba66e154faa77c82c8a524cf47117db3f
Anastasiia Tabalova <anastasiia869@bailliegifford.com>
2025-03-28 15:03:33 +0000

    k8s provider

--- providers.tf
+++ providers.tf
@@ -3,3 +3,10 @@ provider "azurerm" {
   subscription_id                 = "bdbe8cd3-fcbe-49e5-8c57-df0c049cf67a"
   resource_provider_registrations = "none"
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
